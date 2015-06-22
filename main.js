6514a878120c63f1af94ed49f7b4b83b88d74384
secret_online <SecretOnline@users.noreply.github.com>
2015-06-22 18:09:46 +1200

    Add JSON exporting

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ function init(){
     addAddButton(arr[i]);
   var button = document.getElementById('genMD');
   button.addEventListener('click', function(){showModal(generateMarkdown());});
+  button = document.getElementById('genJSON');
+  button.addEventListener('click', function(){showModal(generateJSON());});
+  button = document.getElementById('importJSON');
+  button.addEventListener('click', function(){importJSON();});
 }
 function addInput(parent){
   'use strict';
@@ -50,6 +54,90 @@ function addRemoveButton(parent){
   button.classList.add('remove');
   button.addEventListener('click', function(){removeInput(this.parentNode);});
   parent.appendChild(button);
+}
+function generateJSON(){
+  var obj = {};
+  obj._v = '1.0'; // JSON Version, in case structure changes later
+  obj.beacon = document.getElementById('beacon').value;
+  obj.planet = document.getElementById('planetName').value;
+  obj.star = document.getElementById('starName').value;
+  obj.region = document.getElementById('region').value;
+  obj.discoverer = document.getElementById('discoverer').value;
+  obj.atmosphere = [];
+  arr = document.querySelectorAll('#atmosphere input');
+  for (var i = 0; i < arr.length; i++)
+    obj.atmosphere[i] = arr[i].value;
+  obj.general = {};
+  obj.general.space = [];
+  var arr = document.querySelectorAll('#spaceFeatures input');
+  for (var i = 0; i < arr.length; i++)
+    obj.general.space[i] = arr[i].value;
+  obj.general.ground = [];
+  arr = document.querySelectorAll('#ground input');
+  for (var i = 0; i < arr.length; i++)
+    obj.general.ground[i] = arr[i].value;
+  obj.general.aquatic = [];
+  arr = document.querySelectorAll('#water input');
+  for (var i = 0; i < arr.length; i++)
+    obj.general.aquatic[i] = arr[i].value;
+  obj.plants = {};
+  obj.plants.ground = [];
+  var arr = document.querySelectorAll('#groundPlants .detail');
+  for (var i = 0; i < arr.length; i++){
+    obj.plants.ground[i] = {};
+    obj.plants.ground[i].name = arr[i].querySelector('.name').value;
+    obj.plants.ground[i].image = arr[i].querySelector('.image').value;
+    obj.plants.ground[i].general = [];
+    var bullets = arr[i].querySelectorAll('.input input');
+    for (var j = 0; j < bullets.length; j++)
+      obj.plants.ground[i].general[j] = bullets[j].value;
+  }
+  obj.plants.tree = [];
+  var arr = document.querySelectorAll('#trees .detail');
+  for (var i = 0; i < arr.length; i++){
+    obj.plants.tree[i] = {};
+    obj.plants.tree[i].name = arr[i].querySelector('.name').value;
+    obj.plants.tree[i].image = arr[i].querySelector('.image').value;
+    obj.plants.tree[i].general = [];
+    var bullets = arr[i].querySelectorAll('.input input');
+    for (var j = 0; j < bullets.length; j++)
+      obj.plants.tree[i].general[j] = bullets[j].value;
+  }
+  obj.plants.aquatic = [];
+  var arr = document.querySelectorAll('#waterPlants .detail');
+  for (var i = 0; i < arr.length; i++){
+    obj.plants.aquatic[i] = {};
+    obj.plants.aquatic[i].name = arr[i].querySelector('.name').value;
+    obj.plants.aquatic[i].image = arr[i].querySelector('.image').value;
+    obj.plants.aquatic[i].general = [];
+    var bullets = arr[i].querySelectorAll('.input input');
+    for (var j = 0; j < bullets.length; j++)
+      obj.plants.aquatic[i].general[j] = bullets[j].value;
+  }
+  obj.animals = {};
+  obj.animals.ground = [];
+  var arr = document.querySelectorAll('#groundAnimals .detail');
+  for (var i = 0; i < arr.length; i++){
+    obj.animals.ground[i] = {};
+    obj.animals.ground[i].name = arr[i].querySelector('.name').value;
+    obj.animals.ground[i].image = arr[i].querySelector('.image').value;
+    obj.animals.ground[i].general = [];
+    var bullets = arr[i].querySelectorAll('.input input');
+    for (var j = 0; j < bullets.length; j++)
+      obj.animals.ground[i].general[j] = bullets[j].value;
+  }
+  obj.animals.aquatic = [];
+  var arr = document.querySelectorAll('#waterAnimals .detail');
+  for (var i = 0; i < arr.length; i++){
+    obj.animals.aquatic[i] = {};
+    obj.animals.aquatic[i].name = arr[i].querySelector('.name').value;
+    obj.animals.aquatic[i].image = arr[i].querySelector('.image').value;
+    obj.animals.aquatic[i].general = [];
+    var bullets = arr[i].querySelectorAll('.input input');
+    for (var j = 0; j < bullets.length; j++)
+      obj.animals.aquatic[i].general[j] = bullets[j].value;
+  }
+  return JSON.stringify(obj);
 }
 function generateMarkdown(){
   var md = '#Beacon Number: ' + document.getElementById('beacon').value + '<br>#Planet Name: ' + document.getElementById('planetName').value + '<br>#Star Name: ' + document.getElementById('starName').value + '<br>#Region: ' + document.getElementById('region').value + '<br>#Discoverer: ' + document.getElementById('discoverer').value + '<br><br>##Space Features: <br><br>';
@@ -139,7 +227,6 @@ function generateMarkdown(){
   }
   return md;
 }
-
 function hideModal(){
   'use strict';
   var oldDetail = document.querySelector('#modalContainer');
@@ -147,7 +234,6 @@ function hideModal(){
     oldDetail.parentNode.removeChild(oldDetail);
   }
 }
-
 function showModal(text){
   'use strict';
   hideModal();
@@ -155,11 +241,12 @@ function showModal(text){
   var close = document.createElement('button');
   close.innerHTML = 'Close';
   close.type = 'button';
+  close.id = 'close';
   close.addEventListener('click', hideModal);
   var quote = document.createElement('blockquote');
   quote.innerHTML = text;
   var help = document.createElement('p');
-  help.innerHTML = 'The following text has been generated from the information provided. Something wrong? Submit an issue on <a href="https://github.com/SecretOnline/NMS-Report">GitHub</a>'
+  help.innerHTML = 'Something wrong? Submit an issue on <a href="https://github.com/SecretOnline/NMS-Report">GitHub</a>'
   var modal = document.createElement('div');
   modal.id = 'modal';
   modal.appendChild(close);
@@ -169,6 +256,10 @@ function showModal(text){
   modalContainer.id = 'modalContainer';
   modalContainer.appendChild(modal);
   document.querySelector('body').appendChild(modalContainer);
+}
+
+function importJSON(){
+  showModal("Unfortunately, JSON importing is not yet available. It should be here Soon<sup>tm</sup>, just like No Man's Sky itself... :(");
 }
 
 window.addEventListener('load', init);
